1f44fe2f3528fdeb3bfed7b99a628dfa9e22d5fd
Rezapu Obed <obed.ubaidillah@gmail.com>
2023-05-05 22:18:27 +0700

    feat: seperate iso from non iso date

--- src/query/classifier.js
+++ src/query/classifier.js
@@ -1,9 +1,10 @@
 const QueryClassification = {
     QA: 1,
     calculator: 2,
-    date: 3,
+    dateISO: 3,
     addQA: 4,
-    removeQA: 5
+    removeQA: 5,
+    date: 6
 }
 
 /**
@@ -12,12 +13,14 @@ const QueryClassification = {
  * @returns {number} QueryClasification enum
  */
 const classify = (query) => {
+    const dateISO_regex = new RegExp(/^(\d{1,4}\/\d{1,2}\/\d{1,2})$/);
     const date_regex = new RegExp(/^(\d{1,2}\/\d{1,2}\/\d{1,4})$/);
     const addQA_regex = new RegExp(/^(Tambahkan pertanyaan)\s+(\w+\s*)+\s(dengan jawaban)\s+(\w+\s*)+$/);
     const removeQA_regex = new RegExp(/^(Hapus pertanyaan)\s+(\w+\s*)+$/);
     const calculator_regex = new RegExp(/^\s*((\s*[+\-*/^()]\s*|\d)+\s*[=?]?$)/);
 
-    if(query.match(date_regex)) return QueryClassification.date;
+    if(query.match(dateISO_regex)) return QueryClassification.dateISO;
+    else if(query.match(date_regex)) return QueryClassification.date;
     else if(query.match(calculator_regex)) return QueryClassification.calculator;
     else if(query.match(addQA_regex)) return QueryClassification.addQA;
     else if(query.match(removeQA_regex)) return QueryClassification.removeQA;
